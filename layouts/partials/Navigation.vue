0f2bf57eb078aa07b14635289ea73e8897d8ae56
Abdurrahman Faid <abdurrahman144211@gmail.com>
2020-07-05 06:17:22 +0200

    ~

---
+++ layouts/partials/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- Menu toggle -->
+    <a href="#menu" id="menuLink" class="menu-link">
+      <!-- Hamburger icon -->
+      <span></span>
+    </a>
+
+    <div id="menu">
+      <div class="pure-menu">
+        <a class="pure-menu-heading" href="#">JAMSTACK Test</a>
+
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item"><nuxt-link :to="'/posts'" class="pure-menu-link">Posts</nuxt-link></li>
+          <li class="pure-menu-item"><nuxt-link :to="'/about'" class="pure-menu-link">About</nuxt-link></li>
+          <li class="pure-menu-item"><nuxt-link :to="'/team'" class="pure-menu-link">Team</nuxt-link></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <center>
+      <h3 v-for="post in posts" :key="post.id">
+        <nuxt-link :to="{name: 'posts-slug', params: {slug: post.id}}">{{post.title}}</nuxt-link>
+      </h3>
+    </center>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        posts: []
+      }
+    },
+    async asyncData({app}) {
+      let response =  await app.$axios.$get('https://jsonplaceholder.typicode.com/posts');
+      return {
+        posts: response
+      }
+    }
+  }
+</script>
